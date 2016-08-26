e618a3e907555cb1c5158f6aa2c52c063826cbdb
wooji03 <right98@hanmail.net>
2016-08-26 20:31:06 +0900

    - first Custom Adater

--- 04_ListView/app/src/main/java/com/example/b/a04_listview/MainActivity.java
+++ 04_ListView/app/src/main/java/com/example/b/a04_listview/MainActivity.java
@@ -15,11 +15,12 @@ public class MainActivity extends AppCompatActivity {
             "Hello","World","Orcale","Java",
             "Hello","World","Orcale","Java",
             "Hello","World","Orcale","Java"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    //android.R.layout.simple_expandable_list_item_1,
+        //android.R.layout.simple_expandable_list_item_1,
         //android가 붙으면 안드로이드 시스템영역..없으면 레이어영역
         //AppData\Local\Android\sdk\platforms\android-23\data\res\layout
         ListView listView = (ListView) findViewById(R.id.listView);
