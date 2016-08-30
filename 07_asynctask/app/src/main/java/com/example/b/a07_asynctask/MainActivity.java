4511f279c3a61a90ddac605fef7e6813a84de599
wooji03 <right98@hanmail.net>
2016-08-30 21:00:37 +0900

    -first commit 20160829 1

---
+++ 07_asynctask/app/src/main/java/com/example/b/a07_asynctask/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.b.a07_asynctask;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+
+        MyAsyncTask task = new MyAsyncTask(textView);
+        task.execute(10);
+    }
+}
---
+++ 07_asynctask/app/src/main/java/com/example/b/a07_asynctask/MyAsyncTask.java
@@ -0,0 +1,47 @@
+package com.example.b.a07_asynctask;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MyAsyncTask extends AsyncTask<Integer, Integer, String> {
+    TextView textView;
+
+    public MyAsyncTask(TextView textView) {
+        this.textView = textView;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        textView.setText("count : "+values[0]);
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        textView.setText(s);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(Integer... params) {
+        int start = params[0];
+        for(int i=start; i<100; i++){
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Log.d("Asynctask", "count : "+i);
+//                textView.setText("count : "+i);
+            publishProgress(i);
+        }
+
+        return "AsyncTask done!!!";
+    }
+}
