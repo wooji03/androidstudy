a9c90358ca97e493a6b59b0ca9f38b99bef6353b
wooji03 <right98@hanmail.net>
2016-09-01 21:40:40 +0900

    - first commit 20160901 1 SQLliteOpenHelper - first commit 20160901 2 DBHandler - first commit 20160901 3 생성

--- 12_SqlLite/app/src/main/java/com/example/b/a12_sqllite/MainActivity.java
+++ 12_SqlLite/app/src/main/java/com/example/b/a12_sqllite/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.b.a12_sqllite;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        TestDBHandler testDBHandler = new TestDBHandler(this);
+        testDBHandler.insert("kim","서울",20);
+        testDBHandler.insert("jang","인천",29);
+        testDBHandler.insert("park","경기",33);
+
+        String strRet = testDBHandler.showAllData();
+        textView.setText(strRet);
+
+        testDBHandler.delete("park");
+
+        String strRet2 = testDBHandler.showAllDataResult();
+        textView.setText(strRet2);
+        //Sugar ORM
+        //realm realm.io//자체디비를 쓰게 되고
+        //
+
     }
 }
--- 12_SqlLite/app/src/main/java/com/example/b/a12_sqllite/TestDBHandler.java
+++ 12_SqlLite/app/src/main/java/com/example/b/a12_sqllite/TestDBHandler.java
@@ -2,7 +2,9 @@ package com.example.b.a12_sqllite;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 /**
  * Created by b on 2016-09-01.
@@ -11,6 +13,7 @@ public class TestDBHandler {
     TestSQLliteOpenHelper helper;
 
     public TestDBHandler(Context context){
+        //github clone복사 사용...해보기
         //디비를만든다...
         helper = new TestSQLliteOpenHelper(context,"people",null,1);
     }
@@ -26,11 +29,58 @@ public class TestDBHandler {
         db.insert("student",null,values);
     }
 
+    public void update(String name,String newAge){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("age",newAge);
+        db.update("student",values,"name=?",new String[]{name});
+    }
+
     public void delete(String name){
         SQLiteDatabase db = helper.getWritableDatabase();
         //?에 대응하는게 조건이다.
-        db.delete("student","name = ?", new String[]{name});
+        db.delete("student","name=?", new String[]{name});
     }
+
+    public String showAllData(){
+        String strRs="";
+        //select * form student where id = 1;
+        SQLiteDatabase db = helper.getReadableDatabase();
+        //cursor BOF EOF
+        // db.query( 테이블이름, * , null은 전부를 말한다.
+        Cursor cs = db.query("student",null,null,null,null,null,null);
+        while (cs.moveToNext())
+        {
+            int id = cs.getInt(cs.getColumnIndex("id"));
+            String name = cs.getString(cs.getColumnIndex("name"));
+            String address = cs.getString(cs.getColumnIndex("address"));
+            int age = cs.getInt(cs.getColumnIndex("age"));
+            Log.d("sqlite",id+". name : "+name+", address : "+address+",age : "+age);
+            strRs += id+". name : "+name+", address : "+address+",age : "+age+"\n";
+        }
+        return strRs;
+    }
+
+    public String showAllDataResult(){
+
+        //select * form student where id = 1;
+        String strRs="";
+        SQLiteDatabase db = helper.getReadableDatabase();
+        //cursor BOF EOF
+        // db.query( 테이블이름, * , null은 전부를 말한다.
+        Cursor cs = db.query("student",null,null,null,null,null,null);
+        while (cs.moveToNext())
+        {
+            int id = cs.getInt(cs.getColumnIndex("id"));
+            String name = cs.getString(cs.getColumnIndex("name"));
+            String address = cs.getString(cs.getColumnIndex("address"));
+            int age = cs.getInt(cs.getColumnIndex("age"));
+            Log.d("sqlite",id+". name : "+name+", address : "+address+",age : "+age);
+            strRs += id+". name : "+name+", address : "+address+",age : "+age+"\n";
+        }
+        return strRs;
+    }
+
 
 
 }
