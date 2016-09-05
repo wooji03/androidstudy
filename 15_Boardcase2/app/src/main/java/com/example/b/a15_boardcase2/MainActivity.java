0d2c9d9bcfa67d3dd8abb705ba1169e492bd4e3a
wooji03 <right98@hanmail.net>
2016-09-05 21:51:50 +0900

    - commit Message 20160905 - commit Message 20160905 -2 Change state Toast

--- 15_Boardcase2/app/src/main/java/com/example/b/a15_boardcase2/MainActivity.java
+++ 15_Boardcase2/app/src/main/java/com/example/b/a15_boardcase2/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.b.a15_boardcase2;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0);
+            Toast.makeText(MainActivity.this, "Battery Level : "+level, Toast.LENGTH_LONG).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(receiver,intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(receiver);
     }
 }
