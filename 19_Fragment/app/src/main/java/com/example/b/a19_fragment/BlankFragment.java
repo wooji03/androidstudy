d4ebc488c54e50ab6eb3e37bc9dffe980fe3b9c9
wooji03 <right98@hanmail.net>
2016-09-07 19:51:16 +0900

    -first commit 20160907 -first commit 20160907 -2

--- 19_Fragment/app/src/main/java/com/example/b/a19_fragment/BlankFragment.java
+++ 19_Fragment/app/src/main/java/com/example/b/a19_fragment/BlankFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -22,8 +24,21 @@ public class BlankFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+
+        View v= inflater.inflate(R.layout.fragment_blank, container, false);
+        final TextView textView = (TextView) v.findViewById(R.id.textViewCount);
+        Button btnIcrease = (Button) v.findViewById(R.id.btnIcrease);
+        btnIcrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int val = Integer.parseInt(textView.getText().toString());
+                val++;
+                textView.setText(""+val);
+            }
+        });
+        return v;
+
     }
 
 }
