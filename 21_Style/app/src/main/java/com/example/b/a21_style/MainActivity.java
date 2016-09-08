5c475ac89fb466cbdec0634f12945c8fb135212a
wooji03 <right98@hanmail.net>
2016-09-08 21:05:10 +0900

    - first commit 20160908

--- 21_Style/app/src/main/java/com/example/b/a21_style/MainActivity.java
+++ 21_Style/app/src/main/java/com/example/b/a21_style/MainActivity.java
@@ -9,5 +9,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //C:\Users\b\AppData\Local\Android\sdk\platforms\android-23\data\res\drawable-xxhdpi\ cling_bg.9.png
+        // cling_bg.9.png 9는 나이피치 로 사용한다는 뜻으로 안드로이드는 인식한다.
+        //drawable에 복사후 클릭해보면 나인피치 영역과 컨텐츠 영역을 확인과 영역을 지정할수 있다.
+
+
     }
 }
